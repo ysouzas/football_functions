10e4b2e31b3e27e06b5f9d881ae89a26d0baf661
Santos <YagoSouza.Santos@finastra.com>
2023-08-10 16:17:21 +0100

    update

--- football_functions/Extensions/RankExtensions.cs
+++ football_functions/Extensions/RankExtensions.cs
@@ -42,7 +42,7 @@ public static class RankExtensions
 
         count = yearRanks.Count;
 
-        if (count >= 8)
+        if (count >= 16)
             return Math.Round(yearRanks.Sum(r => r.Score) / count, 2);
 
         return Math.Round(ranks.Sum(r => r.Score) / ranks.Length, 2);
