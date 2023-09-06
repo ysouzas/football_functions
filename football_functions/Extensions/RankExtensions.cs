7f2e91fcd5150c8f2eaedf0d5b90ef7ebdee8378
Santos <YagoSouza.Santos@finastra.com>
2023-09-06 10:59:59 +0100

    update

--- football_functions/Extensions/RankExtensions.cs
+++ football_functions/Extensions/RankExtensions.cs
@@ -20,31 +20,9 @@ public static class RankExtensions
         if (ranks.Length == 0)
             return 0;
 
-        var dateTime = new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+        var eightLastRanks = ranks.OrderByDescending(c => c.Date).Take(8).ToList();
+        var count = eightLastRanks.Count;
 
-        var oneMonthAgoDate = dateTime.AddMonths(-1);
-
-        var oneMonthRanks = ranks.Where(r => r.DateOnlyGeneral() > oneMonthAgoDate).OrderBy(c => c.Date).ToList();
-        var count = oneMonthRanks.Count;
-
-        if (count >= 8)
-            return Math.Round(oneMonthRanks.Sum(r => r.Score) / count, 2);
-
-        var twoMonthsAgoDate = dateTime.AddMonths(-2);
-
-        var twoMonthsRanks = ranks.Where(r => r.DateOnlyGeneral() > twoMonthsAgoDate).OrderBy(c => c.Date).ToList();
-        count = twoMonthsRanks.Count;
-
-        if (count >= 8)
-            return Math.Round(twoMonthsRanks.Sum(r => r.Score) / count, 2);
-
-        var yearRanks = ranks.Where(r => r.DateOnlyGeneral().Year == dateTime.Year).OrderBy(c => c.Date).ToList();
-
-        count = yearRanks.Count;
-
-        if (count >= 16)
-            return Math.Round(yearRanks.Sum(r => r.Score) / count, 2);
-
-        return Math.Round(ranks.Sum(r => r.Score) / ranks.Length, 2);
+        return Math.Round(eightLastRanks.Sum(r => r.Score) / count, 2);
     }
 }
