90c4d358d080040fb5db0ade55771b8667a6fb14
Santos <YagoSouza.Santos@finastra.com>
2023-09-18 09:59:44 +0100

    update

--- football_functions/Extensions/RankExtensions.cs
+++ football_functions/Extensions/RankExtensions.cs
@@ -20,10 +20,10 @@ public static class RankExtensions
         if (ranks.Length == 0)
             return 0;
 
-        var tenLastRanks = ranks.OrderByDescending(c => c.Date).Take(12).ToList();
+        var tenLastRanks = ranks.OrderByDescending(c => c.Date).Take(10).ToList();
         var count = tenLastRanks.Count;
 
-        if (count >= 12)
+        if (count >= 10)
         {
             tenLastRanks = tenLastRanks.OrderBy(r => r.Score).ToList();
             tenLastRanks.RemoveAt(0);
